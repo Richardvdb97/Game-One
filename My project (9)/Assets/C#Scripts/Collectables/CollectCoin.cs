8788363e7388feb4e4b298334e59c41ab9cdbcfd
Richard <136013833+Richardvdb97@users.noreply.github.com>
2023-09-22 19:57:13 +0200

    Coins Added

---
+++ My project (9)/Assets/C#Scripts/Collectables/CollectCoin.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoin : MonoBehaviour
+{
+    public AudioSource coinFX;
+    void OnTriggerEnter(Collider other)
+    {
+        coinFX.Play();
+        this.gameObject.SetActive(false);
+    }
+}
