3baf979ef90860bfd8783ca0c5b601d3428f3701
Richard <136013833+Richardvdb97@users.noreply.github.com>
2023-09-23 21:51:13 +0200

    Audio fitted

---
+++ My project (9)/Assets/C#Scripts/Collectables/LevelDistance.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DistanceRan : MonoBehaviour
+{
+    public GameObject disDisplay;
+    public int disRun;
+    public bool addingDis = false;
+    public float disDelay = 0.35f;
+    
+    void Update()
+    {
+        if(addingDis == false)
+        {
+            addingDis = true;
+            StartCoroutine(AddingDis());
+        }
+    }
+
+    IEnumerator AddingDis()
+    {
+        disRun += 1;
+        disDisplay.GetComponent<TextMeshProUGUI>().text = "" + disRun;
+        yield return new WaitForSeconds(disDelay);
+        addingDis = false;
+    }
+}
---
+++ My project (9)/Assets/C#Scripts/Environment/ObstacleCollision.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleCollision : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public GameObject charModel;
+    void OnTriggerEnter(Collider other)
+    {
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        thePlayer.GetComponent<PlayerMoveR>().enabled = false;
+        charModel.GetComponent<Animator>().Play("Falling Back Death");
+    }
+}
